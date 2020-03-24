f54be7fd756cc9b054c9c482556b6e39898c3546
Il`ya Semyonov <Ilya@marshal.by>
2020-03-24 10:49:11 +0300

    Исправлено название поля с ссылкой на источник в классе Description

--- yandex_music/artist/description.py
+++ yandex_music/artist/description.py
@@ -15,26 +15,27 @@ class Description(YandexMusicObject):
 
     Attributes:
         text (:obj:`str`): Описание исполнителя.
-        url (:obj:`str`): Ссылка на источник.
+        uri (:obj:`str`): Ссылка на источник.
         client (:obj:`yandex_music.Client`): Клиент Yandex Music.
 
     Args:
         text (:obj:`str`): Описание исполнителя.
-        url (:obj:`str`): Ссылка на источник.
+        uri (:obj:`str`): Ссылка на источник.
         client (:obj:`yandex_music.Client`, optional): Клиент Yandex Music.
         **kwargs: Произвольные ключевые аргументы полученные от API.
     """
 
     def __init__(self,
                  text: str,
-                 url: str,
+                 uri: str,
                  client: Optional['Client'] = None,
                  **kwargs) -> None:
         self.text = text
-        self.url = url
+
+        self.uri = uri
 
         self.client = client
-        self._id_attrs = (self.text, self.url)
+        self._id_attrs = (self.text, self.uri)
 
     @classmethod
     def de_json(cls, data: dict, client: 'Client') -> Optional['Description']:
