26d2d773f3a29249cb7fb830582ae1a63c471ae0
Il`ya Semyonov <Ilya@marshal.by>
2021-02-01 12:46:36 +0100

    Добавлен пример работы с радио

---
+++ examples/radio_example/README.md
@@ -0,0 +1,14 @@
+# Пример работы с радио
+
+Документация:
+- [rotor_station_tracks] (https://yandex-music.readthedocs.io/ru/latest/yandex_music.client.html#yandex_music.Client.rotor_station_tracks) – 
+  Получение цепочки треков определённой станции. Читайте примечание.
+  
+Примеры:
+- [radio.py] (radio.py) – класс-обёртка над методами клиента Яндекс.Музыка. 
+  Отправка фидбека о начале и завершении трека, обновление пачек треков.
+- [radio_example.py] (radio_example.py) – пример использования класса-обёртки 
+  для радио.
+- [stream_example.py] (stream_example.py) – пример использования класса-обёртки
+  для запуска потока (похожих) по треку, альбому и исполнителю.
+
---
+++ examples/radio_example/radio_example.py
@@ -0,0 +1,30 @@
+from math import floor
+from random import random
+from time import sleep
+
+from yandex_music import Client
+
+from radio import Radio
+
+# API instance
+client = Client(token="YOUR_TOKEN_HERE")
+
+# Get random station
+_stations = client.rotor_stations_list()
+_station_random_index = floor(len(_stations) * random())
+_station = _stations[_station_random_index].station
+_station_id = f'{_station.id.type}:{_station.id.tag}'
+_station_from = _station.id_for_from
+
+# Radio instance
+radio = Radio(client)
+
+# start radio and get first track
+first_track = radio.start_radio(_station_id, _station_from)
+print("[Radio] First track is:", first_track)
+
+# get new track every 5 sec
+while True:
+    sleep(5)
+    next_track = radio.play_next()
+    print("[Radio] Next track is:", next_track)
