3593ec6eb5db2f9a67894d998c8f6bdc46149473
GREQUE273 <34404990+GREQUE273@users.noreply.github.com>
2024-01-28 20:47:22 +0300

    Fix get_page_async search.py
    Fix get_page_async. next_page_async and prev_page_async dont work without it

--- yandex_music/search/search.py
+++ yandex_music/search/search.py
@@ -92,7 +92,7 @@ class Search(YandexMusicObject):
         Returns:
             :obj:`yandex_music.Search` | :obj:`None`: Страница результата поиска или :obj:`None`.
         """
-        return await self.client.search(self.text, self.nocorrect, self.type_, page, *args, **kwargs)
+        return await self.client.search(self.text, self.nocorrect, self.type, page, *args, **kwargs)
 
     def next_page(self, *args, **kwargs) -> Optional['Search']:
         """Получение следующей страницы поиска.
