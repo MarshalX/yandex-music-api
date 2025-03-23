a2af399414fd0fe7b0ed81a38a17e0a5838e1836
Danil Ovchinnikov <danil42worker@gmail.com>
2025-03-23 19:14:22 +0300

    Поддержка Python 3.13

--- setup.py
+++ setup.py
@@ -54,11 +54,12 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    python_requires='~=3.8',
+    python_requires='>=3.8',
     cmdclass={'test': PyTest},
     tests_require=['pytest'],
     project_urls={
