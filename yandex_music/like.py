aadc6b3caa24bce955f53d363327a864a2243acf
Ilya (Marshal) <ilya@marshal.dev>
2025-05-16 22:09:20 +0200

    Исправлен метод `de_list` для класса `List`

--- yandex_music/like.py
+++ yandex_music/like.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Sequence
 
 from yandex_music import Album, Artist, Playlist, YandexMusicModel
 from yandex_music.utils import model
@@ -55,8 +55,8 @@ class Like(YandexMusicModel):
 
         Args:
             data (:obj:`dict`): Поля и значения десериализуемого объекта.
-            type_ (:obj:`str`, optional): Тип объекта с отметкой "мне нравится".
             client (:obj:`yandex_music.Client`, optional): Клиент Yandex Music.
+            type_ (:obj:`str`, optional): Тип объекта с отметкой "мне нравится".
 
         Returns:
             :obj:`yandex_music.Like`: Объект с отметкой "мне нравится".
@@ -80,3 +80,26 @@ class Like(YandexMusicModel):
         cls_data['type'] = type_
 
         return cls(client=client, **cls_data)  # type: ignore
+
+    @classmethod
+    def de_list(cls, data: JSONType, client: 'ClientType', type_: Optional[str] = None) -> Sequence['Like']:
+        """Десериализация списка объектов.
+
+        Note:
+            Переопределяется в дочерних классах, если необходимо.
+
+            Например, в сложных объектах где есть вариации подтипов.
+
+        Args:
+            data (:obj:`JSONType`): Список словарей с полями и значениями десериализуемого объекта.
+            client (:obj:`yandex_music.Client`, optional): Клиент Yandex Music.
+            type_ (:obj:`str`, optional): Тип объекта с отметкой "мне нравится".
+
+        Returns:
+            :obj:`list` из :obj:`yandex_music.Like`: Список десериализованных объектов.
+        """
+        if not cls.is_array_model_data(data):
+            return []
+
+        items = [cls.de_json(item, client, type_) for item in data]
+        return [item for item in items if item is not None]
